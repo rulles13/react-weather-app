ae3037b06a7e73455a04d154b0955954b1050ac8
Stephane Gillon <stephanegillon@hotmail.com>
2022-04-12 16:30:22 +0200

    get API openweathermap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState } from "react"
+import axios from 'axios'
+import './App.css'
 
 function App() {
+  const location = useRef()
+
+  function getWeather () {
+    axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${location.current.value}&APPID=${process.env.REACT_APP_API_KEY}`)
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" ref={location} required placeholder="Select a place"></input>
+      <button onClick={getWeather}> Get weather </button>
     </div>
   );
 }
